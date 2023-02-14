4ff00eb4979460e6a3b7ef7d1695e9c43405167a
Camilo <camilo.riquelme@outlook.es>
2023-02-14 12:50:23 -0300

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,43 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer"
+
+const init = () => JSON.parse( localStorage.getItem('todos')) || [];
+
+export const useTodo = ({initialState}) => {
+        
+    const [todos, dispatch] = useReducer( todoReducer, initialState, init);
+
+    const handleNewTodo = ( todo ) => dispatch({
+        type: 'Add Todo',
+        payload: todo
+    });
+
+    const handleDeleteTodo = ( id ) => dispatch({
+        type: 'Remove Todo',
+        payload: id
+    });
+      
+    const handleToggleTodo = ( id ) => dispatch({
+        type: 'Toggle Todo',
+        payload: id
+    });
+
+    const todosCounter = () => todos.length;
+
+    const pendingTodosCount = () => todos.filter(p => !p.done).length;
+
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify( todos ) || []);
+    }, [todos]);
+
+    return {
+        todos,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        todosCounter,
+        pendingTodosCount
+
+    }
+}
